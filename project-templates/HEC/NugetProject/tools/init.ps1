875abcf44d758873a723dcdcd3b7c6a67f2cfb90
Sven Sieverding <sven@usetheco.de>
2016-06-06 16:02:02 +0200

    Initial checkin

---
+++ project-templates/HEC/NugetProject/tools/init.ps1
@@ -0,0 +1,6 @@
+﻿# Uncomment the following to automatically load a $safeprojectname$.dll into the package Manager Console
+#param($installPath, $toolsPath, $package)
+#Import-Module (Join-Path $toolsPath $safeprojectname$.dll)
+#Write-Host "#############################"
+#Write-Host "$safeprojectname$ installed."
+#Write-Host "#############################"
